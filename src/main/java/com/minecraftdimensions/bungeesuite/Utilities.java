afbf0ad56d98783fe682eaf3ff13ecb4d4c06e99
Andireas <andif@live.dk>
2020-01-14 14:22:03 +0100

    Changed to IntelliJ project

---
+++ src/main/java/com/minecraftdimensions/bungeesuite/Utilities.java
@@ -0,0 +1,15 @@
+package com.minecraftdimensions.bungeesuite;
+
+import com.google.common.net.InetAddresses;
+import net.md_5.bungee.api.ChatColor;
+
+public class Utilities {
+	
+	public static boolean isIPAddress(String ip){
+		return InetAddresses.isInetAddress(ip);
+	}
+
+	public static String colorize(String input) {
+		return ChatColor.translateAlternateColorCodes('&', input);
+	}
+}
